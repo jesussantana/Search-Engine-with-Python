20280be452d71be1ccfb7b17761a71dd7c748f2a
jesussantana <jesus@esmarketingdigital.com>
2021-05-23 16:11:52 +0200

    Project Organization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build A Search Engine With Python',
+    author='Jesus Santana',
+    license='MIT',
+)
